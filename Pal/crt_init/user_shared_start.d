2ed953e6630292663d84976bbd0455f00628e7fe
eniomarku <51482113+eniomarku@users.noreply.github.com>
2021-01-17 23:27:42 +0100

    Delete Pal directory

--- Pal/crt_init/user_shared_start.d
+++
@@ -1,3 +0,0 @@
-user_shared_start.o: user_start-x86_64.S /usr/include/stdc-predef.h
-
-/usr/include/stdc-predef.h:
--- Pal/src/host/Linux/.obj/pal_error.d
+++
@@ -1,9 +0,0 @@
-host/Linux/.obj/pal_error.o: pal_error.c /usr/include/stdc-predef.h \
- ../include/pal/pal_error.h \
- /usr/lib/gcc/x86_64-linux-gnu/7/include/stddef.h
-
-/usr/include/stdc-predef.h:
-
-../include/pal/pal_error.h:
-
-/usr/lib/gcc/x86_64-linux-gnu/7/include/stddef.h:
