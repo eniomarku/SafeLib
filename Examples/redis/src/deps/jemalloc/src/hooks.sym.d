5a46de5946f98ca8e6377cf63640bb53b65c420f
eniomarku <51482113+eniomarku@users.noreply.github.com>
2021-01-17 23:45:43 +0100

    Delete Examples directory

--- Examples/redis/src/deps/jemalloc/src/hooks.sym.d
+++
@@ -1,7 +0,0 @@
-src/hooks.sym.o: src/hooks.c \
- include/jemalloc/internal/jemalloc_preamble.h \
- include/jemalloc/internal/jemalloc_internal_defs.h \
- include/jemalloc/internal/jemalloc_internal_decls.h \
- include/jemalloc/internal/../jemalloc.h \
- include/jemalloc/internal/jemalloc_internal_macros.h \
- include/jemalloc/internal/hooks.h
--- Examples/redis/src/src/ae.d
+++
@@ -1,2 +0,0 @@
-ae.o: ae.c ae.h zmalloc.h ../deps/jemalloc/include/jemalloc/jemalloc.h \
- config.h ae_epoll.c
