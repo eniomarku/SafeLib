ea140eeb7b194c83747aac52c016e57d142e5aa7
eniomarku <51482113+eniomarku@users.noreply.github.com>
2021-02-25 16:13:16 +0100

    Delete LibOS directory

--- LibOS/shim/include/shim_ipc_sysv.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: LGPL-3.0-or-later */
-
-#ifndef _SHIM_IPC_SYSV_H_
-#define _SHIM_IPC_SYSV_H_
-
-int init_ns_sysv(void);
-void debug_print_sysv_ranges(void);
-
-#endif /* _SHIM_IPC_SYSV_H_ */
